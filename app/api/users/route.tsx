ae4795a2db1f3f75de4c7df7bf2813bca080d0fe
mostafa <mostafameerzad@gmail.com>
2024-09-21 11:04:34 +0430

    insert data in db using prisma

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,7 +1,7 @@
 // a route handler is a function that handles an http request
 
 import { NextRequest, NextResponse } from "next/server";
-import schema from "./[id]/schema";
+import schema from "./schema";
 import prisma from "@/prisma/client";
 // export a get function for handling get request
 
@@ -10,15 +10,14 @@ import prisma from "@/prisma/client";
 export async function GET(request: NextRequest) {
   // return NextResponse.json("hello")
   // return NextResponse.json([
-    // { id: 1, name: "John" },
-    // { id: 2, name: "Sam" },
+  // { id: 1, name: "John" },
+  // { id: 2, name: "Sam" },
   // ]);
 
-
   // now instead of hard coding the user objects we query the DB to get the users
-  const users =await prisma.user.findMany()
-  console.log(users)
-  return NextResponse.json(users)
+  const users = await prisma.user.findMany();
+  console.log(users);
+  return NextResponse.json(users);
 }
 
 // to handle post requests export a function with POST to handle post requests
@@ -32,10 +31,26 @@ export async function POST(request: NextRequest) {
   // todo
   // validate the data we get
   // if invalid return 400
-  // else, return the data
+  // else, create an entry in DB and return the data
   // ------------
   // validation using zod
-  const validation = schema.safeParse(body)
-  if (!validation.success) return NextResponse.json( validation.error.errors , {status: 400});
-  return NextResponse.json({ id: 11, name: body.name }, {status: 201});// 201 to indicate an object was created
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  // return NextResponse.json({ id: 11, name: body.name }, {status: 201});// 201 to indicate an object was created
+
+  const user = await prisma.user.findUnique({where:{ email: body.email}});
+
+  if (user) {
+    return NextResponse.json({ error: "user already exists" }, { status: 400 });
+  }
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(newUser, { status: 201 });
 }
