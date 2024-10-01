ae9abdd7da67814af665dcdc20a9c0d46b1cb29d
mostafa <mostafameerzad@gmail.com>
2024-10-01 10:14:59 +0430

    accessing sessions on the server

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,24 @@
 import Link from "next/link";
 import AddToChart from "./components/AddToCard";
 import ProductCard from "./components/ProductCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
     <main>
-      <h1>Hello Next.js</h1>
+      {/* <h1>Hello Next.js</h1> */}
+      {
+        <h1>
+          Hello <span>{session && session.user!.name}</span>
+        </h1>
+      }
       {/* using a normal "a" tag in routing causes all the resources to be fetched again */}
       {/* <a href="/users">users</a> */}
       {/* to fix it use "Link" component defined in next.js */}
       <Link href={"/users"}>users</Link>
-      <ProductCard/>
+      <ProductCard />
     </main>
-  )
+  );
 }
